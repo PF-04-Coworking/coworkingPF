e16410bc92111296f349c6c63cd913f1153053da
renatamontoya <renatasalgadolozoyar@gmail.com>
2024-07-25 15:59:28 -0600

    definicion de guards

---
+++ back/src/user/guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+      throw new UnauthorizedException('Token not provided');
+    }
+ 
+    try {
+      const payload = await this.jwtService.verifyAsync(token);
+      request.user = payload;
+      return true;
+    } catch (e) {
+      throw new UnauthorizedException('Invalid token');
+    }
+  }
+}
