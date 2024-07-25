476c911c2ad5a8614e6bac322d53dd5943d8a433
Mateo Tello Alfaro <mateotelloalfaro@gmail.com>
2024-07-25 11:59:26 -0500

    Revison de carpetas

--- back/src/offices/offices.module.ts
+++ back/src/offices/offices.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OfficeController } from 'src/offices/offices.controller';
 import { Office } from 'src/entities/Offices.entity';
-import { OfficeRepository } from 'src/repositories/offices.repository';
+import { OfficeRepository } from 'src/offices/offices.repository';
 import { OfficeService } from 'src/offices/offices.service';
 
 @Module({
