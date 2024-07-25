27174b222de4ef662789589a92266cb90ecffca1
unknown <lolacamposcipo@gmail.com>
2024-07-25 14:03:14 -0300

    CRUD de users con SignUp y SingIn

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -6,11 +6,13 @@ import { User } from './entities/Users.entity';
 import { Office } from './entities/Offices.entity';
 import { Reservation } from './entities/Reservations.entity';
 import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
+import { UserRepository } from './user/user.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Office, Reservation])],
+  imports: [TypeOrmModule.forFeature([User, Office, Reservation]), UserModule],
   controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  providers: [AppService, UserService, UserRepository],
 })
 export class AppModule {}
