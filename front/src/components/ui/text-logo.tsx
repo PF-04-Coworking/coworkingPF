6f24ab052e3bc786cf4d10a54a02af2721c5c3c2
BardoUntiveros <bardountiveros@gmail.com>
2024-07-25 23:32:05 -0500

    Nuevos componentes

---
+++ front/src/components/ui/text-logo.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import textLogo from "@/../public/text-logo.png";
+
+interface IProps extends React.ButtonHTMLAttributes<HTMLImageElement> {
+  className?: string;
+}
+
+const TextLogo = ({ className, ...props }: IProps) => {
+  const baseClassName = "h-10";
+
+  return (
+    <Image src={textLogo} alt="Logo" className={baseClassName} {...props} />
+  );
+};
+
+export { TextLogo };
