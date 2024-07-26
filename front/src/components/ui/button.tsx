df23999004019fe66bd2a72e130200fc3cf1777a
BardoUntiveros <bardountiveros@gmail.com>
2024-07-25 23:29:32 -0500

    Correcciones a estilo de variantes de botones

--- front/src/components/ui/button.tsx
+++ front/src/components/ui/button.tsx
@@ -7,11 +7,12 @@ interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
 const Button = ({ children, className, variant, ...props }: IProps) => {
   const baseClassName =
-    "inline-flex items-center justify-center font-medium rounded-md focus:outline-none px-4 py-2 transition transition-colors";
+    "inline-flex items-center justify-center font-medium rounded-md focus:outline-none px-4 py-2 transition transition-colors text-sm";
   const variantClassName = {
     primary:
       "border border-primary hover:border-primaryDark bg-primary text-white hover:bg-primaryDark text-white",
-    outline: "border border-primary bg-transparent hover:bg-primary text-white",
+    outline:
+      "border border-primary bg-transparent hover:text-primary text-white",
   };
   const finalClassName = clsx(
     baseClassName,
