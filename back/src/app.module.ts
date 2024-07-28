e5da8f796c2ce62b634de8ca6d8bfcd89a6f9c00
Felipe Cabana <cabanita2390@hotmail.com>
2024-07-27 23:21:56 -0500

    Algunas correcciones de conflictos

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -8,14 +8,10 @@ import { Reservation } from './entities/Reservations.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { typeOrmConfig } from './Config/typeorm';
 import { OfficeModule } from './offices/offices.module';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
-import { UserService } from './user/user.service';
-import { UserRepository } from './user/user.repository';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { ReservationsModule } from './reservations/reservations.module';
 import { FileUploadModule } from './file-upload/file-upload.module';
-// import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -42,19 +38,6 @@ import { FileUploadModule } from './file-upload/file-upload.module';
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '60m' },
     }),
-    // AuthModule,
-    // JwtModule.registerAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: async (configService: ConfigService) => {
-    //     const jwtSecret = configService.get<string>('JWT_SECRET');
-
-    //     return {
-    //       secret: jwtSecret,
-    //       signOptions: { expiresIn: '1h' },
-    //     };
-    //   },
-    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
