0eda0328b33de02740e07bd14c325ffcccd704ab
BardoUntiveros <bardountiveros@gmail.com>
2024-08-04 21:29:02 -0500

    Añadido ruta "/api"

--- front/src/lib/api/apiConfig.ts
+++ front/src/lib/api/apiConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_DOMAIN,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_DOMAIN + "/api",
 });
 
 export { axiosClient };
